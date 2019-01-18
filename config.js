3302bb928ceb857b0452d2b72c343bcfa5783efc
Matthew-MK <44151355+Matthew-MK@users.noreply.github.com>
2019-01-18 14:27:07 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 config.maxBots = 50;//Default=50. If using VPS, max=300 or lag. If not using VPS, max=100 or lag.
-config.serverPort = 8081;//Default=8081
+config.serverPort = process.env.PORT || 3000;//Default=8081
 config.proxies = "proxy.txt";//Default=proxy.txt
 config.chatMsg = "bit.ly/mkbots";//Default=Free Snow at www.olaf4snow. com
 
